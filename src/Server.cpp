a8de10de50d05081c1f86582b696ce88c70b5fb1
VArtz <farrelnikoson@gmail.com>
2024-06-01 15:24:04 +0700

    'test'

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 
+const int HEADER_SIZE = 100;
 int main(int argc, char* argv[]) {
     // Flush after every std::cout / std::cerr
     std::cout << std::unitbuf;
@@ -30,6 +31,13 @@ int main(int argc, char* argv[]) {
         unsigned short page_size = (static_cast<unsigned char>(buffer[1]) | (static_cast<unsigned char>(buffer[0]) << 8));
         
         std::cout << "database page size: " << page_size << std::endl;
+
+        database_file_path.seekg(HEADER_SIZE + 3);
+        database_file.read(buffer, 2);
+
+        unsigned short table_count = (static_cast<unsigned char>(buffer[1] | static_cast<unsigned char>(buffer[0] << 8));
+
+        std::cout << "number of tables: " << table_count << std::endl;
     }
 
     return 0;
